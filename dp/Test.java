53806a68fb315de95b983a14f2634379a06d7373
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-02-18 18:28:13 +0900

    testing and setting

---
+++ dp/Test.java
@@ -0,0 +1,15 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TEST");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}

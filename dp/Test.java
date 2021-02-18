729074586ef89aa37335b9c9333f216d823f2616
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-02-18 21:08:38 +0900

    solve 2 dp problem

--- dp/Test.java
+++ dp/Test.java
@@ -1,14 +1,10 @@
 package dp;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("TEST");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("git test");
 		System.out.println("dd");
 	}

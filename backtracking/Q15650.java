c84e6f9020067b44553c8857e978dd9ef4866ba3
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-03-03 00:24:57 +0900

    Q15650_backtracking

--- backtracking/Q15650.java
+++ backtracking/Q15650.java
@@ -12,7 +12,7 @@ public class Q15650 {
     public static int N;
     public static int M;
     public static int[] numbers;
-    public static int[] check;
+    public static boolean[] check;
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -21,14 +21,29 @@ public class Q15650 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         numbers = new int[M];
-        check = new int[N + 1];
-
-        solution(0);
+        check = new boolean[N + 1];
+        for(int i = 1 ;i<N+1-(M-1);i++){
+            numbers[0] = i;
+            solution(i,0);
+        }
+        System.out.print(sb.toString());
     }
 
-    public static void solution(int count) {
-        if (count == M) {
+    public static void solution(int start, int count) {
+       if(count == M-1){
+           for(int n : numbers){
+               sb.append(n).append(" ");
+           }
+           sb.append("\n");
+           return;
+       }
+       if(N-start < M-count-1){
+        return;
+       }
 
+        for(int i = start+1; i<N+1;i++){
+            numbers[count+1] = i;
+            solution(i,count+1);
         }
     }
 }

d42bca94aba576c258798f9cad32afd9c0421d96
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-03-04 18:44:07 +0900

    bactracking 1-th end & DP start

--- backtracking/Q14889.java
+++ backtracking/Q14889.java
@@ -1,17 +1,70 @@
 package backtracking;
 
-public class Q14889 {
-    // 123
-    // 12 13 21 23 31 32
-    // 456?
-    // 45 46 54 56 64 65를 구하면 너무 늦다.
-//팀을 짜고 고를 필요가 없다
-// 오름차순으로 팀을 짜면서 바로바로 더하면 됨
-// 근데 나머지는?
-//오름차순으로 팀짜면서 점수 바로 구하고
-// 정해진 팀원의 나머지 점수를 다 빼버리면? 안되요
-//123이면
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-    public static void main(String[] args) {
+public class Q14889 {
+    static int[][] ability;
+    static boolean[] team;
+    static int N;
+    static int result = Integer.MAX_VALUE;
+    static int half;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        ability = new int[N][N];
+        team = new boolean[N];
+        half = N >> 1;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; ++j) {
+                ability[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        team[0] = true;
+        solution(0, 1);
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void solution(int person_num, int count) {
+        if (count == half) {
+            result = Math.min(result, get_Answer());
+            return;
+        }
+
+        for (int i = person_num + 1; i < N; i++) {
+            team[i] = true;
+            solution(i, count + 1);
+            team[i] = false;
+        }
+    }
+
+    public static int get_Answer() {
+        int start_team = 0;
+        int link_team = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (team[i] && team[j])
+                    start_team += ability[i][j];
+                else if (!team[i] && !team[j])
+                    link_team += ability[i][j];
+            }
+        }
+
+        return Math.abs(start_team - link_team);
     }
 }
---
+++ dp/Q9461.java
@@ -0,0 +1,32 @@
+/** int로 했는데 될거같은데 안되면 long 
+ * 문제에서 long파악은 힘든거 같은데?
+ * 다른예로
+ * dp[i-2] + dp[i-3]도 존재함.
+ */
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9461 {
+    static long[] dp = new long[101];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        dp[0] = 0L;
+        dp[1] = 1L;
+        dp[2] = 1L;
+        dp[3] = 1L;
+        dp[4] = 2L;
+        for (int i = 5; i < 101; i++) {
+            dp[i] = dp[i - 5] + dp[i - 1];
+        }
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}

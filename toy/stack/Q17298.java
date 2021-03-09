a40fda4fc9bb181731b0fe5e465bfa8ca9bb0aae
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-03-09 22:49:01 +0900

    stack start

--- toy/stack/Q17298.java
+++ toy/stack/Q17298.java
@@ -1,3 +1,6 @@
+/* https://st-lab.tistory.com/196
+https://www.acmicpc.net/problem/17298
+*/
 package toy.stack;
 
 import java.io.BufferedReader;
@@ -7,42 +10,19 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Q17298 {
-    static Stack<Integer> data;
+    static Stack<Integer> OCE;
     static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int tlqkf[];
-        data = new Stack<>();
-        tlqkf = new int[N];
+        int data[] = new int[N];
+        OCE = new Stack<>();
         sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            tlqkf[i] = Integer.parseInt(st.nextToken());
+            data[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = N - 1; i >= 0; i--) {
-            data.push(tlqkf[i]);
-        }
-        while (!data.isEmpty()) {
-            int x = data.pop();
-            System.out.println("pop" + x);
-            find(x);
-        }
-        System.out.println(sb.toString());
-    }
 
-    public static void find(int x) {
-        if (!data.isEmpty()) {
-            int temp = data.pop();
-            System.out.println("pop temp " + temp);
-            if (temp <= x)
-                find(x);
-            else
-                sb.append(temp + " ");
-            data.push(temp);
-        } else {
-            sb.append(-1 + " ");
-        }
     }
 }

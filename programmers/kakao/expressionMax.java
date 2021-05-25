88d5ef0b92d063902c1185cf82b73a5c37222c3f
pcrmcw0486 <pcrmcw0486@gmail.com>
2021-05-25 16:32:59 +0900

    카카오 인턴 2020수식최대화

--- programmers/kakao/expressionMax.java
+++ programmers/kakao/expressionMax.java
@@ -3,17 +3,15 @@ package programmers.kakao;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class expressionMax {
-    static boolean[] priority = new boolean[3];
-    static int[] operatorPriority;
+    static Long ans = 0L;
     static ArrayList<Long> operand;
     static ArrayList<Character> operator;
-    static Set<Character> operatorSet = new HashSet<>();
+    static Set<Character> resultSet;
+    static Character[] operatorSet;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,38 +20,66 @@ public class expressionMax {
     }
 
     public static long solution(String expression) {
-        long answer = 0;
+        // 정규식을 통한 구분
+        operand = new ArrayList<>();
+        operator = new ArrayList<>();
+        resultSet = new HashSet<>();
+        operand.addAll(Arrays.stream(expression.split("[*,\\-,+]")).mapToLong(Long::parseLong).boxed()
+                .collect(Collectors.toList()));
+        operator.addAll(
+                expression.replaceAll("[0-9]", "").trim().chars().mapToObj(e -> (char) e).collect(Collectors.toList()));
+        resultSet = expression.replaceAll("[0-9]", "").trim().chars().mapToObj(e -> (char) e)
+                .collect(Collectors.toSet());
+        operatorSet = new Character[resultSet.size()];
+        resultSet.toArray(operatorSet);
 
-        StringBuilder sb = new StringBuilder(5);
-        // data setting
-        for (int i = 0; i < expression.length(); i++) {
-            char c = expression.charAt(i);
-            if (Character.isDigit(c)) {
-                sb.append(c);
-            } else {
-                if (!operatorSet.contains(c)) {
-                    operatorSet.add(c);
+        int[] visit = new int[operatorSet.length];
+        comb(1, visit);
+
+        return ans;
+    }
+
+    private static void comb(int idx, int[] visit) {
+        if (idx == operatorSet.length) {
+            ans = Math.max(ans, calcProgress(visit));
+        }
+        for (int i = 0; i < operatorSet.length; i++) {
+            if (visit[i] == 0) {
+                visit[i] = idx;
+                comb(idx + 1, visit);
+                visit[i] = 0;
+            }
+        }
+    }
+
+    private static long calcProgress(int[] operatorPriority) {
+        ArrayList<Long> tmpOperand = new ArrayList<>();
+        ArrayList<Character> tmpOperator = new ArrayList<>();
+        tmpOperand.addAll(operand);
+        tmpOperator.addAll(operator);
+        for (int i = 0; i < operatorSet.length; i++) {
+            for (int j = 0; j < tmpOperator.size(); j++) {
+                if (operatorSet[operatorPriority[i]] == tmpOperator.get(j)) {
+                    long op1 = tmpOperand.get(j);
+                    long op2 = tmpOperand.get(j + 1);
+                    Character oper = tmpOperator.get(j);
+                    tmpOperand.remove(j);
+                    tmpOperand.remove(j);
+                    tmpOperand.add(j, calc(op1, op2, oper));
+                    tmpOperator.remove(j);
+                    j--;
                 }
-                operator.add(c);
-                operand.add(Long.valueOf(sb.toString()));
-                sb.delete(0, sb.length());
             }
         }
-        operatorPriority = new int[operator.size()];
-        answer = comb(operatorSet.size(), 0);
-
-        return answer;
+        return Math.abs(tmpOperand.get(0));
     }
 
-    public static comb(int size, int idx){
-        if(size == idx){
-            return calc();
-        }
-        for(int i =0;i<size;i++){
-            if(!priority[i]){
-                
-            }
-        }
+    private static long calc(long op1, long op2, Character oper) {
+        if (oper == '+')
+            return op1 + op2;
+        else if (oper == '-')
+            return op1 - op2;
+        else
+            return op1 * op2;
     }
-
 }
